72f44f05858f1b502a731466cec9c94072308c80
kosoepaing <mgpaingsoe.ps@gmail.com>
2024-08-08 17:28:35 +0630

    LoginOptions Screen

--- Recipe/Recipe/Application/SceneDelegate.swift
+++ Recipe/Recipe/Application/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let personalizeVC = PersonalizeOptionVC()
-        window.rootViewController = personalizeVC
+        let loginVC = LoginOptionsVC()
+        window.rootViewController = loginVC
         self.window = window
         window.makeKeyAndVisible()
     }
---
+++ Recipe/Recipe/Views/LoginOptions/LoginOptionsVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginOptionsVC.swift
+//  Recipe
+//
+//  Created by user on 08/08/2024.
+//
+
+import UIKit
+
+class LoginOptionsVC: UIViewController {
+
+    // @IBOutlet weak var btnEmailLogin: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @objc func setupEmailLoginScreen() {
+        print("Next Page")
+    }
+    
+
+}
